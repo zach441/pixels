654f680840e16bf8ee43cf6808876a5d0fff21ac
Zach Oakes <zachoakes96@gmail.com>
2016-07-19 18:29:51 -0500

    Now you can pass filenames as command line args

--- main.c
+++ main.c
@@ -15,12 +15,37 @@
 //#define dry_run
 
 int main(int argc, char** argv) {
+    char* inName = malloc(64);
+    char* outName = malloc(64);
+    //64 characters should be enouth for a file name, maybe change later -z
+   
+    if(argc != 1) {
+        switch(argc) {
+            case 2:
+                //todo: adda a way to get to a help message
+                inName = argv[1];
+                outName = "out.bmp";
+                break;
+            case 3: 
+                inName = argv[1];
+                outName = argv[2];
+                break;
+            default:
+                puts("you did too many args, how did you even???");
+                //todo make this go to ahelp message
+                exit(0);
+        }
+
+    } else {
+        inName = "in.bmp";
+        outName = "out.bmp";
+    }
     setbuf(stdout, NULL);
     FILE* inF;
-    inF = inInit("in.bmp");
+    inF = inInit(inName);
 #ifndef dry_run
     FILE* outF;
-    outF = outInit("out.bmp");
+    outF = outInit(outName);
 #endif //dry_run
     unsigned int fileEnd = getSize(inF);
     unsigned int dataStart = getStart(inF);
@@ -42,6 +67,7 @@ int main(int argc, char** argv) {
 #endif //debug_mode
 #ifndef dry_run
     makeImage(header, image, outF);
+
     fclose(outF);
 #endif// dry_run
     fclose(inF);
